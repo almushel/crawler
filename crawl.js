89ccfdce4f594286ee448ca4846155c41d6d7841
Andrew Mushel <almushel@protonmail.com>
2023-11-01 15:02:49 -0500

    Add getURLsFromHTML

--- crawl.js
+++ crawl.js
@@ -1,8 +1,25 @@
+const {JSDOM} = require("jsdom");
+
 function normalizeURL(url) {
-	const result = new URL(url)
-	return result.hostname;
+	try {
+		const result = new URL(url);
+		return result.hostname;
+	} catch (e) {
+		console.log(e.message);
+		return url;
+	}
 }
 
+function getURLsFromHTML(htmlBody, baseURL) {
+	let result = [];
+	const dom = new JSDOM(htmlBody);
+	const links = dom.window.document.querySelectorAll("a")
+	for (let link of links) {
+		result.push(`${baseURL}${link.href}`);
+	}
+
+	return result;
+}
 module.exports = {
-	normalizeURL
+	normalizeURL, getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,5 +1,5 @@
 const {test, expect} = require("@jest/globals")
-const {normalizeURL} = require("./crawl")
+const {normalizeURL, getURLsFromHTML} = require("./crawl")
 
 test(
 	"removes https://", () => {
@@ -16,5 +16,35 @@ test(
 test(
 	"removes http:// and trailing slash", () => {
 		expect(normalizeURL("http://www.test.com/")).toBe("www.test.com")
+	} 
+)
+
+const HTML_BODY = `
+	<html>
+	<head></head>
+
+	<a href="/test/"></a>	
+	<a href="/test/"></a>	
+	<a href="/test/"></a>
+	<a href="/test/"></a>
+	
+	</html>	
+`
+const urlList = [
+	"test.com/test/",
+	"test.com/test/",
+	"test.com/test/",
+	"test.com/test/",
+]
+
+test(
+	"gets absolute links from HTML", () => {
+		expect(getURLsFromHTML(HTML_BODY, "test.com")).toEqual(urlList);
+	} 
+)
+
+test(
+	"gets ALL links from HTML", () => {
+		expect(getURLsFromHTML(HTML_BODY, "test.com").length).toBe(4);
 	} 
 )
